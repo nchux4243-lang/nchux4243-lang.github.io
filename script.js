f8ac6c8e3f93b601ed9a2cd472f214c9c094a3fa
nchux4243-lang <nchux4243@gmail.com>
2026-08-25 09:06:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('year').innerHTML =new Date().getFullYear();
     console.log('Portfolio ของ KING พร้อมแล้ว! ' ) ;
 });
